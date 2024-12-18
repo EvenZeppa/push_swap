2df0ca8e1616b11cf5eac9033d36c69079dc4df0
ezeppa <ezeppa@Debian.myguest.virtualbox.org>
2024-12-18 23:13:26 +0100

    UPD libft.h

--- include/ft_lib_conversion.h
+++
@@ -1,8 +0,0 @@
-#ifndef FT_LIB_CONVERSION_H
-# define FT_LIB_CONVERSION_H
-
-int		ft_atoi(const char *nptr);
-char	*ft_itoa(int n);
-char	*ft_litoa(long n);
-
-#endif
--- include/ft_lib_number.h
+++
@@ -1,6 +0,0 @@
-#ifndef FT_LIB_NUMBER_H
-# define FT_LIB_NUMBER_H
-
-long	long_size(long n);
-
-#endif
